f20df637cb53feb86fbe68c5a68d98c58777bef1
LAMRI Rayane <34587541+RayaneLamri@users.noreply.github.com>
2021-12-02 15:30:48 +0100

    Create index.php

---
+++ content/hooks/actions/plugin/index.php
@@ -0,0 +1,9 @@
+<?php 
+
+/*
+** Plugin Name: WP Post Statistics
+** Description: Provide you some stats about the article you're about to read.
+** Version: 1.0
+** Author: Rayane
+** Author URI: https://www.github.com/RayaneLamri/
+*/

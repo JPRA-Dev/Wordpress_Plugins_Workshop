21ea5ee38544a5eacb5079815a648169372fb4e2
LAMRI Rayane <34587541+RayaneLamri@users.noreply.github.com>
2021-12-06 07:42:10 +0100

    Delete content/hooks/actions directory

--- content/hooks/actions/plugin/index.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-/*
-** Plugin Name: WP Post Statistics
-** Description: Provide you some stats about the article you're about to read.
-** Version: 1.0
-** Author: Rayane
-** Author URI: https://www.github.com/RayaneLamri/
-*/
